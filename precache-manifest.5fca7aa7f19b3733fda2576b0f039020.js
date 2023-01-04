89a750b349f9353b4b60284a5b2d4ef5eef4d458
Meena Kuduva <meenakg@cs.washington.edu>
2023-01-03 22:37:34 -0800

    Updates

---
+++ precache-manifest.5fca7aa7f19b3733fda2576b0f039020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6def37bac39f99f43e4d",
+    "url": "/dictionaryAPI/static/css/main.a96959e2.chunk.css"
+  },
+  {
+    "revision": "6def37bac39f99f43e4d",
+    "url": "/dictionaryAPI/static/js/main.6bef48cf.chunk.js"
+  },
+  {
+    "revision": "53988a1f4927d574a93b",
+    "url": "/dictionaryAPI/static/js/runtime~main.f44723a3.js"
+  },
+  {
+    "revision": "96fdc53493ff035961e8",
+    "url": "/dictionaryAPI/static/js/2.6ca77bf0.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/dictionaryAPI/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "77f0097956d539ea53a2e667d8644155",
+    "url": "/dictionaryAPI/index.html"
+  }
+];
